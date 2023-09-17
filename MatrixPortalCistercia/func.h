73a429ae42d78bc23ca3cee7f391cbf7553571bc
poevoid <65972401+poevoid@users.noreply.github.com>
2023-09-16 22:00:43 -0500

    Update func.h

--- MatrixPortalCistercia/func.h
+++ MatrixPortalCistercia/func.h
@@ -145,6 +145,240 @@ void drawCistercian9th(){
   drawCistercian2th()
   drawCistercian6th();
 }
+void drawUnits(){
+  if (counter == 0){
+    drawCistercian0();
+  }
+  if (counter == 1){
+    drawCistercian1();
+  }
+  if (counter ==2){
+    drawCistercian2()
+  }
+  if (counter == 3){
+    drawCistercian3();
+  }
+  if (counter == 4){
+    drawCistercian4();
+  }
+  if (counter == 5){
+    drawCistercian5();
+  }
+  if (counter == 6){
+    drawCistercian6();
+  }
+  if (counter == 7){
+    drawCistercian7();
+  }
+  if (counter == 8){
+    drawCistercian8();
+  }
+  if (counter == 9){
+    drawCistercian9();
+  }
+}
+void drawTens(){
+  if (counter >= 10 && counter < 20){
+    drawCistercian10();
+    counter -=10;
+    drawUnits();
+    counter+=10;
+  }
+  if (counter >= 20 && counter < 30){
+    drawCistercian20();
+    counter -=20;
+    drawUnits();
+    counter+=20;
+  }
+  if (counter >= 30 && counter < 40){
+    drawCistercian30();
+    counter -=30;
+    drawUnits();
+    counter+=30;
+  }
+  if (counter >= 40 && counter < 50){
+    drawCistercian40();
+    counter -=40;
+    drawUnits();
+    counter+=40;
+  }
+  if (counter >= 50 && counter < 60){
+    drawCistercian50();
+    counter -=50;
+    drawUnits();
+    counter+=50;
+  }
+  if (counter >= 60 && counter < 70){
+    drawCistercian60();
+    counter -=60;
+    drawUnits();
+    counter+=60;
+  }
+  if (counter >= 70 && counter < 80){
+    drawCistercian70();
+    counter -=70;
+    drawUnits();
+    counter+=70;
+  }
+  if (counter >= 80 && counter < 90){
+    drawCistercian80();
+    counter -=80;
+    drawUnits();
+    counter+=80;
+  }
+  if (counter >= 90 && counter < 100){
+    drawCistercian90();
+    counter -=90;
+    drawUnits();
+    counter+=90;
+  }
+}
+void drawHundreds(){
+  if (counter >= 100&& counter < 2h){
+    drawCistercian1h();
+    counter -=100;
+    drawUnits();
+    drawTens();
+    counter+=100;
+  }
+  if (counter >= 200&& counter < 300){
+    drawCistercian2h();
+    counter -=200;
+    drawUnits();
+    drawTens();
+    counter+=200;
+  }
+  if (counter >= 300&& counter < 400){
+    drawCistercian3h();
+    counter -=300;
+    drawUnits();
+    drawTens();
+    counter+=300;
+  }
+  if (counter >= 400&& counter < 500){
+    drawCistercian4h();
+    counter -=400;
+    drawUnits();
+    drawTens();
+    counter+=400;
+  }
+  if (counter >= 500&& counter < 600){
+    drawCistercian5h();
+    counter -=500;
+    drawUnits();
+    drawTens();
+    counter+=500;
+  }
+  if (counter >= 600&& counter < 700){
+    drawCistercian6h();
+    counter -=600;
+    drawUnits();
+    drawTens();
+    counter+=600;
+  }
+  if (counter >= 700&& counter < 800){
+    drawCistercian7h();
+    counter -=700;
+    drawUnits();
+    drawTens();
+    counter+=700;
+  }
+  if (counter >= 800&& counter < 900){
+    drawCistercian8h();
+    counter -=800;
+    drawUnits();
+    drawTens();
+    counter+=800;
+  }
+  if (counter >= 900&& counter < 100){
+    drawCistercian9h();
+    counter -=900;
+    drawUnits();
+    drawTens();
+    counter+=900;
+  }
+}
+void drawThousands(){
+  if (counter >= 1000 && counter < 2000){
+    drawCistercian1th();
+    counter -=1000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=1000;
+  }
+  if (counter >= 2th && counter < 3000){
+    drawCistercian2th();
+    counter -=2000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=2000;
+  }
+  if (counter >= 3th && counter < 4000){
+    drawCistercian3th();
+    counter -=3000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=3000;
+  }
+  if (counter >= 4th && counter < 5000){
+    drawCistercian4th();
+    counter -=4000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=4000;
+  }
+  if (counter >= 5th && counter < 6000){
+    drawCistercian5th();
+    counter -=5000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=5000;
+  }
+  if (counter >= 6th && counter < 7000){
+    drawCistercian6th();
+    counter -=6000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=6000;
+  }
+  if (counter >= 7th && counter < 8000){
+    drawCistercian7th();
+    counter -=7000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=7000;
+  }
+  if (counter >= 8th && counter < 9000){
+    drawCistercian8th();
+    counter -=8000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=8000;
+  }
+  if (counter >= 9th && counter < 10000){
+    drawCistercian9th();
+    counter -=9000;
+    drawUnits();
+    drawTens();
+    drawHundreds();
+    counter+=9000;
+  }
+}
+
+void drawCistercian(){
+  drawUnits();
+  drawTens();
+  drawHundreds();
+  drawThousands();
+}
 
 void application() {
   //matrix.setCursor(0,0);
