692ce04b86bfaa5b0a6f516e24272904d01e3828
poevoid <65972401+poevoid@users.noreply.github.com>
2023-09-16 23:44:58 -0500

    Update MatrixPortalCistercia.ino

--- MatrixPortalCistercia/MatrixPortalCistercia.ino
+++ MatrixPortalCistercia/MatrixPortalCistercia.ino
@@ -15,17 +15,23 @@ Adafruit_Protomatter matrix(
 void setup() {
   // put your setup code here, to run once:
   ProtomatterStatus status = matrix.begin();
-   //if (!accel.begin(0x19)) {
-    //Serial.println("Couldn't find accelerometer");
-    ///err(250);  // Fast bink = I2C error
-  //}
-  //accel.setRange(LIS3DH_RANGE_2_G);  // 2, 4, 8 or 16 G!
-  //accel.setClick(2, CLICKTHRESHHOLD);
+  //if (!accel.begin(0x19)) {
+  //Serial.println("Couldn't find accelerometer");
+  ///err(250);  // Fast bink = I2C error
+//}
+//accel.setRange(LIS3DH_RANGE_2_G);  // 2, 4, 8 or 16 G!
+//accel.setClick(2, CLICKTHRESHHOLD);
+#if defined(BACK_BUTTON)
+  pinMode(BACK_BUTTON, INPUT_PULLUP);
+#endif
+#if defined(NEXT_BUTTON)
+  pinMode(NEXT_BUTTON, INPUT_PULLUP);
+#endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-   uint32_t t;
+  uint32_t t;
   while (((t = micros()) - prevTime) < (1000000L / MAX_FPS))
     ;
   prevTime = t;
